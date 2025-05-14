d1fe3dab05183362cc10b1a1864f732e3277fa18
Claudia de Luna <cldeluna@yahoo.com>
2025-05-14 12:12:59 -0700

    03 refactored

---
+++ 03_ConfigurationsMonolithic/README.md
@@ -0,0 +1,80 @@
+# Configuration Templates Monolithic Style
+
+The next two mini projects have a number of examples to showcase the different strategies.
+
+- Monolithic <-- We are here
+- Modular
+  - Using Jinja2 Inheritance
+  - Using Jinja2 Includes
+
+## Monolithic
+
+With the monolitic strategy, templates are define in their entirety based on 
+
+- Vendor
+- function
+- region
+- hardware model
+- location type
+
+This has a simplicity that is very attractive and easy to grasp conceptually as well as in code.
+You have one master template for an access switch, a branch office switch, etc.  Some will have templates based on region, etc.  If you set up your templates and data you can get rid of regional templates (strongly recommend).  The goal is to minimize (erradicate really) redundant data. Also know as DRY "Don't Repeat Yourself"!
+
+Its a great place to start and lends itself to taking your current templates and "Jinjaizing" them.
+
+In the monolitic example you will find a Python script which generates device configurations from the new_switch.csv file using the DNAC (aka Catalyst Center) base sample template which can be found (as ususal) in the templates directory.
+
+```python
+% tree
+.
+├── README.md
+├── cfg_output
+├── gen_monolithic_sw_cfg.py
+├── new_switches.csv
+└── templates
+    ├── dnac_baseconfig_sampleJinjaTemplate.txt
+    ├── dnac_baseconfig_sample_template.j2
+    └── vendor
+        └── cisco
+            ├── aci
+            ├── ios
+            ├── ios-xe
+            ├── ios-xr
+            └── nxos
+
+10 directories, 10 files
+```
+
+This introduces another common strategy, which you saw to some degree in the 02_Procedure mini project.  Bring in payload (REST, JSON, YAML, XLSX, CSV etc.) and then add key/value pairs.
+
+In this case, we want to timestamp each template so we know when it was generated, so we calculate a human readable timestamp and add that key/value pair to our payload dictionarly.
+
+We bring in a temporary enable secret and add that to the payload.
+
+We calculate the number of user interfaces based on the model number so we know how many interfaces to configure.
+
+Full disclosure:  I've cheated here as this template assumes all the user interfaces are the same so the range command can be used.  In real life, you often have specific configurations, if only vlans, for each interface.
+
+```python
+% uv run gen_monolithic_sw_cfg.py --help
+usage: gen_monolithic_sw_cfg.py [-h] [-p PAYLOAD_FILE] [-o OUTPUT_DIR] [-s SECRET_TEMP] [-t TIMEZONE]
+
+Script Description
+
+options:
+  -h, --help            show this help message and exit
+  -p PAYLOAD_FILE, --payload_file PAYLOAD_FILE
+                        CSV Payload file to use Default is new_switches.csv
+  -o OUTPUT_DIR, --output_dir OUTPUT_DIR
+                        output directory for configuration files Default is cfg_output
+  -s SECRET_TEMP, --secret_temp SECRET_TEMP
+                        Temporary enable secret and login password
+  -t TIMEZONE, --timezone TIMEZONE
+                        Timezone defaults to America/Los_Angeles
+
+Usage: ' python monolithic_sw_cfg.py or uv run monolithic_sw_cfg.py # Assumes a CSV file new_switches.csv with new switch payload'
+```
+
+
+
+
---
+++ 03_ConfigurationsMonolithic/gen_monolithic_sw_cfg.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python -tt
+# Project: ac2_templating_workshop
+# Filename: monolithic_sw_cfg
+# claudiadeluna
+# PyCharm
+
+from __future__ import absolute_import, division, print_function
+
+__author__ = "Claudia de Luna (claudia@indigowire.net)"
+__version__ = ": 1.0 $"
+__date__ = "10/9/24"
+__copyright__ = "Copyright (c) 2023 Claudia"
+__license__ = "Python"
+
+import argparse
+import os
+import datetime
+import pytz
+
+
+from utils import utils
+
+
+
+def main():
+
+    # Get the current date and time
+    now = datetime.datetime.now(pytz.timezone(arguments.timezone))  # Replace with your timezone
+
+    # Format it as a string
+    timestamp = now.strftime("%B %d, %Y at %I:%M:%S %p %Z")
+
+    # Date stamp for Report if one already exists
+    file_timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
+    # Load the data in the CSV file
+    payload_data = utils.load_csv(os.path.join(os.getcwd(), arguments.payload_file))
+    print(f"\nGenerating configurations for switches in CSV file {arguments.payload_file}\n")
+
+    if payload_data:
+        # We have read in a CSV file with the first row as the header and then one ore more lines of new switches
+        # Its much better to work with a list of dictionaries
+        payload_lod = utils.lists_to_dicts(payload_data)
+
+        # cfg_dict will be passed to the template as "cfg"
+        for cfg_dict in payload_lod:
+
+            # Build template payload
+            cfg_dict.update({"timestamp": timestamp})
+
+            # Get enable secret
+            cfg_dict.update({"enable_sec": arguments.secret_temp})
+
+            # Determine Max user interfaces from model number
+            # Is this the best way to do this?  No.
+            if "24" in cfg_dict['sw_model']:
+                max_intfs = 24
+            elif "8" in cfg_dict['sw_model']:
+                max_intfs = 8
+            else:
+                max_intfs = 48
+
+            cfg_dict.update({"max_intfs": max_intfs})
+
+            # Determine mask in dotted notation get_mask_from_cidr(cidr)
+            cfg_dict.update({"mgmt_mask": utils.get_mask_from_cidr(cfg_dict['mgmt-subnet_cidr'])})
+
+            # Check to see if the output directory exists and if it does not, create it
+            # This is the directory where we will store the resulting config files
+            cfg_directory = os.path.join(os.getcwd(),arguments.output_dir)
+            utils.check_and_create_directory(cfg_directory)
+            # Create the resulting template filename with timestamp. Using txt so its easy to open with default
+            # editors
+            filename = f"{cfg_dict['hostname']}_{file_timestamp}.txt"
+
+            cfg_file_fullpath = os.path.join(cfg_directory, filename)
+
+            rendered = utils.render_in_one("dnac_baseconfig_sample_template.j2", cfg_dict)
+
+            print(f"\tGenerating configuration for {filename}")
+
+            utils.save_file(cfg_file_fullpath, rendered)
+
+        print(f"\nSaved files in {cfg_directory}\n")
+
+
+# Standard call to the main() function.
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Script Description",
+        epilog="Usage: ' python monolithic_sw_cfg.py or uv run monolithic_sw_cfg.py # Assumes a CSV file new_switches.csv with new switch payload' ",
+    )
+
+    # parser.add_argument('all', help='Execute all exercises in week 4 assignment')
+    parser.add_argument(
+        "-p",
+        "--payload_file",
+        help="CSV Payload file to use Default is new_switches.csv",
+        action="store",
+        default="new_switches.csv",
+    )
+    parser.add_argument(
+        "-o",
+        "--output_dir",
+        help="output directory for configuration files Default is cfg_output",
+        action="store",
+        default="cfg_output",
+    )
+    parser.add_argument(
+        "-s",
+        "--secret_temp",
+        help="Temporary enable secret and login password",
+        action="store",
+        default="T3mp@ecreto",
+    )
+    parser.add_argument(
+        "-t",
+        "--timezone",
+        help="Timezone defaults to America/Los_Angeles",
+        action="store",
+        default="America/Los_Angeles",
+    )
+
+    arguments = parser.parse_args()
+    main()
