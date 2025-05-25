b0b5355314aa9d6d0f3d5c57b0ac6f46a71c54c4
Claudia de Luna <claudia@Claudias-MacBook-AirM415.local>
2025-05-25 19:38:19 +0200

    Added hint

--- 03_ConfigurationsMonolithic/gen_monolithic_sw_cfg_scaffold.py
+++ 03_ConfigurationsMonolithic/gen_monolithic_sw_cfg_scaffold.py
@@ -35,7 +35,7 @@ def main():
 
     print(f"\nGenerating configurations from {arguments.payload_file}\n")
 
-    # TODO: Convert list of rows to list of dictionaries using a utility function
+    # TODO: Convert list of rows to list of dictionaries using a utility function (utils.lists_to_dicts)
     # You have read in a CSV file with the first row as the header and then one ore more lines of new switches
     # Its much better to work with a list of dictionaries
 
